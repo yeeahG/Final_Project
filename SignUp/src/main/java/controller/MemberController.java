112ce2b4c99764b0122444977bd5a789e224a995
JSL107 <juneseok0107@naver.com>
2022-05-30 21:56:00 +0900

    [add] Back-End 틀

---
+++ SignUp/src/main/java/controller/MemberController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+}
--- SignUp/src/main/java/dto/Member.java
+++ SignUp/src/main/java/dto/Member.java
@@ -1,4 +1,10 @@
 package dto;
 
+import lombok.Data;
+
+@Data
 public class Member {
+    private String id;
+    private String pw;
+
 }
