11998e2fe38f1de00bdb6742a75928165ba268b5
JSL107 <juneseok0107@naver.com>
2022-06-02 16:51:49 +0900

    [update] "postman 통신 확인"

--- SignUp/src/main/java/com/example/signup/controller/MemberForm.java
+++ SignUp/src/main/java/com/example/signup/controller/MemberForm.java
@@ -3,12 +3,12 @@ package com.example.signup.controller;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
-public class MemberForm {
-    private String phoneNumber;
-    private String name;
-    private String id;
-    private String password;
-    private String checkPassword;
-}
+//@Getter
+//@Setter
+//public class MemberForm {
+//    private String phoneNumber;
+//    private String name;
+//    private String id;
+//    private String password;
+//    private String checkPassword;
+//}
